885bf68531d33a65c4213d76671696d4e29ac87f
ccastillo18 <ccastillo@cristoreyny.org>
2015-11-19 13:15:14 +0000

    Rock Paper Scissors Project

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,17 +3,38 @@ var aiPoint = 0;
 
 // This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    var game= ["rock", "paper", "scissors"];
+    var Randomoption= Math.floor(Math.random()*3);
+    console.log(Randomoption);
+    return game[Randomoption];
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
+    if(userValue == aiValue){
+        return "draw";
+    }
+    else if(aiValue == "paper" && userValue == "rock"){
+        return "ai";
+    }
+    else if(aiValue == "rock" && userValue == "scissors"){
+        return "ai";
+    }
+    else if(aiValue == "scissors" && userValue == "paper" ){
+        return "ai";
+    }
+    else{
+        return "user";
+    }
+    
     //TODO: Add one point for the winner
 }
 
 // This function sets the scoreboard with the correct points
 function setScore() {
+    $("#userPoint").text(userPoint);
+    $("#aiPoint").text(aiPoint);
+    
 
 }
 
@@ -25,15 +46,22 @@ function evaluate(evt) {
     var winner = pickWinner(userValue, aiValue);
 
     if ( 'user' === winner ) {
-        $('#message').delay(50).text('You have won!, Click a box to play again');
+        $('#message').show("2000", "").delay(50).text('You have won!, Click a box to play again');
+        userPoint++;
     } else if ( winner === 'draw' ) {
-        $('#message').delay(50).text('Draw! Click a box to play again');
+        $('#message').show("2000" , "").delay(50).text('Draw! Click a box to play again');
     } else {
-        $('#message').delay(50).text('You have lost!, Click a box to play again');
+        $('#message').show("2000", "").delay(50).text('You have lost!, Click a box to play again');
+        aiPoint++;
     }
+   setScore();    
 }
 
 // This function runs on page load
 $(document).ready(function(){
-
+    $(".token").click(function(evt){
+        hide(1000);
+        evaluate(evt);
+    });
+    
 });
